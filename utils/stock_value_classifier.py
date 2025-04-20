b913fe6a47fe8f733cf0c2200f018abaf25ed03c
nomnemo <ng53@rice.edu>
2025-04-20 00:34:00 -0500

    cache

--- utils/stock_value_classifier.py
+++ utils/stock_value_classifier.py
@@ -1,13 +1,13 @@
 import yfinance as yf
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
-from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.metrics import confusion_matrix
+import streamlit as st
 
 
 """# Random Forest Classifier for Stock Value Classification
@@ -21,6 +21,7 @@ from sklearn.metrics import confusion_matrix, classification_report
 """
 
 # This function creates a DataFrame with the P/E, P/B, and P/S ratios for all S&P 500 stocks
+@st.cache_data(ttl=86400)  # cache for 24 hours
 def get_sp500_metrics():
   # Extract all S&P500 stocks
   metrics = pd.DataFrame()
